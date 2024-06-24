778a5248cbaff4fee989bf04be14fb8f3042cd75
notourist <notourist@posteo.net>
2024-06-24 18:15:15 +0200

    adwhIUAWHDIUAHW 2

--- src/bin/query_reader.rs
+++ src/bin/query_reader.rs
@@ -1,12 +1,13 @@
+use bitvec::vec::BitVec;
+use memmap2::Mmap;
 use runaway_datastructures::query::{Query, QueryResult};
 use runaway_datastructures::runaway_vector::RunawayVector;
 use std::fs::{File, OpenOptions};
-use std::io::{Write};
+use std::io::Write;
+use std::os::unix::fs::MetadataExt;
 use std::path::Path;
 use std::time::Instant;
 use std::{env, io};
-use bitvec::vec::BitVec;
-use memmap2::Mmap;
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
@@ -56,14 +57,19 @@ const SIZE: usize = 1074000000;
 fn read_file(file: File) -> io::Result<(Vec<Query>, BitVec<u64>)> {
     let mut read_state = ReadState::FirstLine;
     let mmap = unsafe { Mmap::map(&file)? };
+    let size = if file.metadata().unwrap().size() as usize > SIZE {
+        SIZE
+    } else {
+        file.metadata().unwrap().size() as usize
+    };
     let mut queries: Vec<Query> = Vec::new();
-    let mut bit_vec = BitVec::with_capacity(SIZE);
+    let mut bit_vec = BitVec::with_capacity(size);
     let mut tmp_query = String::new();
     let mut page_count = 0;
-    let page_count_end = mmap.len() / SIZE;
+    let page_count_end = mmap.len() / size;
 
     while page_count < page_count_end {
-        let page = &mmap[page_count * SIZE..(page_count + 1) * SIZE];
+        let page = &mmap[page_count * size..(page_count + 1) * size];
         for i in 0..page.len() {
             match read_state {
                 ReadState::FirstLine => {
@@ -89,7 +95,7 @@ fn read_file(file: File) -> io::Result<(Vec<Query>, BitVec<u64>)> {
                     }
                 }
             }
-        };
+        }
         page_count += 1;
     }
     Ok((queries, bit_vec))
