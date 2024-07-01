26ad70dd342bd6a7114040beb51826212bcc43e2
notourist <notourist@posteo.net>
2024-07-01 12:48:05 +0200

    blub2

--- benchi/hetzner.sh
+++ benchi/hetzner.sh
@@ -1,1 +1,1 @@
-apt update && apt upgrade && apt install gcc rustup && rustup install default && cargo build --release && ./generate.sh && ./run.sh
+apt -y update && apt -y upgrade && apt -y install gcc rustup && rustup install default && cargo build --release && ./generate.sh && ./run.sh
