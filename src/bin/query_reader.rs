518ab6b1a56e3b9e356997ea9e4e94cc2433967e
notourist <notourist@posteo.net>
2024-07-02 15:20:09 +0200

    wadawd

--- src/bin/query_reader.rs
+++ src/bin/query_reader.rs
@@ -38,7 +38,7 @@ fn main() -> Result<(), io::Error> {
     }
     let read_elapsed = start.elapsed();
 
-    let vector = NaiveVector::new(&bit_vec, 512);
+    let vector = NaiveVector::new(&bit_vec, 1024);
     let build_elapsed = start.elapsed();
 
     let results: Vec<QueryResult> = queries.iter().map(|query| vector.process(query)).collect();
